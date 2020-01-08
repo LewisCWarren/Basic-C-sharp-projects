e1c5aa1ac347139949df966ce1907731d6645e93
Lewis <lewis.c.warren@gmail.com>
2020-01-08 10:44:12 -0800

    Add shipping project to repository

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int roomTemp = 70;
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + name + ", what is the temp where you are?");
+            int currentTemp = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemp == roomTemp)
+            {
+                Console.WriteLine("It is exactly room temperature");    
+
+            }   
+
+            else
+            {
+                Console.WriteLine("It is not room temperature");
+            }
+
+            Console.ReadLine();
+
+
+
+            //int currentTemp = 70;
+            //int roomTemp = 70;
+
+            //string comparison = currentTemp == roomTemp ? "It is room Temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparison);
+
+            //if (currentTemp == roomTemp)
+
+            //{
+            //    Console.WriteLine("It is exactly room temp.");
+            //    Console.ReadLine();
+            //}
+
+            //else
+
+            //{
+            //    Console.WriteLine("Its not room temp currently.");
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ ShippingQuoteProject/ShippingQuoteProject/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuoteProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+            Console.WriteLine("Please enter package weight");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            string refusal = packageWeight > 50 ? "Package is too heavy to be shipped via package express" : "Please enter package width";
+
+            Console.WriteLine(refusal);
+            
+            
+
+            if (packageWeight > 50)
+            {
+                Environment.Exit(0);
+            }
+
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter package height");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Package length?");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+
+            int packageDimensions = packageWidth + packageHeight + packageLength;
+
+            int price = packageDimensions * packageWeight;
+
+            if (packageDimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via package express.");
+                Environment.Exit(0);
+            }
+
+            else
+            {
+                Console.WriteLine("The price to ship this packe will be " + price + ".00");
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+        }
+    }
+}
