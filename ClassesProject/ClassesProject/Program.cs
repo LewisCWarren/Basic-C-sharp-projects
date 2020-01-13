de9373d0c3440847e2abda1a6db04a25d6c85ba1
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-13 15:56:45 -0800

    Add changes to project.

--- ClassesProject/ClassesProject/Program.cs
+++ ClassesProject/ClassesProject/Program.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace ClassesProject
 {
-    class Program
+   public class Program
     {
         public static void Main(string[] args)
         {
-
+            Math mathOperation = new Math();
+            
             Console.WriteLine("Please enter a number to perform the operation on");
             int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("After adding 100, subtracting 33, and dividing by 7, the number you entered amounts to" + mathOperation.Result(userNum));
+            Console.ReadLine();
         }
     }
 }
--- ClassesProject/ClassesProject/ProjectClass.cs
+++ ClassesProject/ClassesProject/ProjectClass.cs
@@ -10,16 +10,36 @@ namespace ClassesProject
     {
 
 
-        public Math()
+        public int Addition(int userNum)
         {
-            Add = 100;
-            Subtract = 33;
-            Divide = 7;
+            
+            Add = 100 + userNum;
+            return Add;
         }
 
         int Add { get; set; }
+
+        public int Subtraction( int Add)
+        {
+            Subtract = 33 - Add;
+            return Subtract;
+        }
+
         int Subtract { get; set; }
+
+        public int Division (int Subtract)
+        {
+            Divide = Subtract / 7;
+            return Divide;
+        }
+
         int Divide { get; set; }
-        
+
+        public int Total()
+        {
+            Result = Add + Subtract + Divide;
+            return Result;
+        }
+        int Result { get; set; }
     }
 }
