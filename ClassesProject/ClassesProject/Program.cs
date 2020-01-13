bbb63fa549148d49d66225fe2b35db076a2262bf
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-13 15:37:09 -0800

    Add new projects to repo

---
+++ ClassesProject/ClassesProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number to perform the operation on");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ ClassesProject/ClassesProject/ProjectClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProject
+{
+    public class Math
+    {
+
+
+        public Math()
+        {
+            Add = 100;
+            Subtract = 33;
+            Divide = 7;
+        }
+
+        int Add { get; set; }
+        int Subtract { get; set; }
+        int Divide { get; set; }
+        
+    }
+}
