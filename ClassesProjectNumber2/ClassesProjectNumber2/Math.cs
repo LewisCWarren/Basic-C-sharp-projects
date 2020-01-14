f26e3216b55288ed31fe20ca6bb4ecea9c3ad27d
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-14 15:10:18 -0800

    Add project from page 152

---
+++ ClassesProjectNumber2/ClassesProjectNumber2/Math.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectNumber2
+{
+    public class Math
+    {
+        public int Addition(int userNumOne)
+        {
+            Add = 25 + userNumOne;
+            return Add;
+        }
+        public int Add { get; set; }
+
+        public decimal Addition(decimal userNumTwo)
+        {
+            multiply = userNumTwo * 3;
+            return multiply;
+        }
+        public decimal multiply { get; set; }
+
+
+        string userString;
+
+
+        public int Conversion( string userString)
+        {
+            int num = Convert.ToInt32(userString);
+            userNum = num * 5;
+            
+            return userNum;
+        }
+        public int userNum { get; set; }
+        public int equation { get; set; }
+        
+    }
+}
---
+++ ClassesProjectNumber2/ClassesProjectNumber2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectNumber2
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Math MathOperation = new Math();
+
+            Console.WriteLine("Please provide a number for the operation");
+            int userNumOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the number you provided + 25 is = to " + MathOperation.Addition(userNumOne));
+            Console.ReadLine();
+            Console.WriteLine("Please enter a decimal number for a different operation");
+            decimal userNumTwo = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("The decimal number provided times 3 is = " + MathOperation.Addition(userNumTwo));
+            Console.ReadLine();
+            Console.WriteLine("Please enter a final number for an operation");
+            string userString = Console.ReadLine();
+            Console.WriteLine("Your input x 5 = " + MathOperation.Conversion(userString));
+            Console.ReadLine();
+        }
+    }
+}
