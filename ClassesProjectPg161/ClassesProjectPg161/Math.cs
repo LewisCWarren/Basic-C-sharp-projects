7675d7b2266b61e060abecc76358f9e701f84338
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-15 10:10:44 -0800

    Add drill from page 161.

---
+++ ClassesProjectPg161/ClassesProjectPg161/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectPg161
+{
+    public class Math
+    {
+       public void  Addition( int userNumOne, int userNumTwo)
+        {
+            Add = userNumOne + 20;
+            String = Convert.ToString(userNumTwo);
+            String2 = Convert.ToString(Add);
+            Console.WriteLine(String);
+            Console.WriteLine(String2);
+            
+
+        }
+        public int Add { get; set; }
+        public string String2 { get; set; }
+        public string String { get; set; }
+    }
+}
---
+++ ClassesProjectPg161/ClassesProjectPg161/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectPg161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathOperation = new Math();
+
+            Console.WriteLine("please enter a number");
+            int userNumOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int userNumTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The first number has had 20 added to it. the second number has been left alone.");
+            mathOperation.Addition(userNumOne, userNumTwo);
+            Console.ReadLine();
+        }
+    }
+}
