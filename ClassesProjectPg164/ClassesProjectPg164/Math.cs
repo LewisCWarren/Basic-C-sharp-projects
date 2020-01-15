ae0f6dbcef6c8569625904746ebed81ebfd1f6bb
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-15 11:28:00 -0800

    Add project for page 164

---
+++ ClassesProjectPg164/ClassesProjectPg164/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectPg164
+{
+    public class Math
+    {
+        public void Division(int userNum)
+        {
+            Divide = userNum / 2;
+            string result = Convert.ToString(Divide);
+            Console.WriteLine(result);
+        }
+        public int Divide { get; set; }
+
+        public int Subtraction(int userNum2)
+        {
+            Subtract = userNum2 - 1;
+            return Subtract;
+        }
+        public int Subtract { get; set; }
+
+        public int Subtraction(int userNum2, int userNum3 = 1)
+        {
+            Subtract = userNum2 - 1;
+            Subtract2 = userNum3 - 1;
+            return Subtract;
+        }
+        int Subtract2 { get; set; }
+    }
+}
---
+++ ClassesProjectPg164/ClassesProjectPg164/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectPg164
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathOperation = new Math();
+
+            Console.WriteLine("Please enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the number provided divided by 2 = ");
+            mathOperation.Division(userNum);
+            Console.ReadLine();
+            
+
+        }
+
+
+    }
+}
