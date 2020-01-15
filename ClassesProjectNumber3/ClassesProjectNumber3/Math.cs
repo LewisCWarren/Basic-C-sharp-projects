fdae1cc1bd1b31fbedc32a2172481a02a573aee9
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-14 16:04:49 -0800

    Add project from page 160.

---
+++ ClassesProjectNumber3/ClassesProjectNumber3/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectNumber3
+{
+   public class Math
+    {
+        public int Addition(int userNumOne, int userNumTwo = 1)
+        {
+            addOne = userNumOne + 35;
+            addTwo = userNumOne + userNumTwo + 35;
+            
+            if (userNumTwo > 1)
+            {
+                return addTwo;
+            }
+            else
+            {
+                return addOne;
+            }
+        }
+        public int addTwo { get; set; }
+        public int addOne { get; set; }
+    }
+}
---
+++ ClassesProjectNumber3/ClassesProjectNumber3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectNumber3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathOperation = new Math();
+            
+            Console.WriteLine("Please enter a number for the math operation");
+            int userNumOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may now enter a second number to perform the operation on, or leave it blank.");
+            string numTwo = Console.ReadLine();
+            if (numTwo == "")
+            {
+                Console.WriteLine("The number / s you provided + 35 = " + mathOperation.Addition(userNumOne));
+            }
+            else
+            {
+                int userNumTwo = Convert.ToInt32(numTwo);
+                Console.WriteLine("The number/s you provided + 35 = " + mathOperation.Addition(userNumOne, userNumTwo));
+            }
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
