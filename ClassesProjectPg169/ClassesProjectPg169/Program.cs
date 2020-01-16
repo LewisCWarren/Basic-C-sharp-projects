01305d5aad18c662ed7c7c34fdefa1543ccd9d4c
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-15 16:05:06 -0800

    Add drill from page 169.

---
+++ ClassesProjectPg169/ClassesProjectPg169/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProjectPg169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "[Student", LastName = "Sample]" };
+
+            
+
+            employee.SayName();
+            Console.ReadLine();
+            
+        }
+    }
+}
