027f00f6172d173a865efa2962b4e0af82c15d09
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-16 12:54:55 -0800

    Add drill from page 176.

---
+++ InheritanceClassProject/InheritanceClassProject/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceClassProject
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+            
+        }
+
+
+
+    }
+}
