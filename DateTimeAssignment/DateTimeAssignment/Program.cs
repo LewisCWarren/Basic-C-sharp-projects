af0477ba4309190b51ff075927979ca239d6eb0c
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-22 14:19:33 -0800

    Add DateTime assignment from page 221.

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            
+            Console.WriteLine("\n Please enter a number.");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine( DateTime.Now.AddHours(userInput));
+            Console.ReadLine();
+            
+
+        }
+    }
+}
