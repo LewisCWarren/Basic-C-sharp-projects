f9e18b58edf5349d2475a219b3a7c8c7320c582c
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-22 13:07:13 -0800

    Add assignment from page 218.

---
+++ System.IOAssignment/System.IOAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace System.IOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            string userNum = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Student\Logs\Assignment.txt", userNum);
+            string readIt = File.ReadAllText(@"C:\Users\Student\Logs\Assignment.txt");
+            Console.WriteLine(readIt);
+            Console.ReadLine();
+        }
+    }
+}
