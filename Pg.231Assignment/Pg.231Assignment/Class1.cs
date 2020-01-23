8e6d7a21d31b981d8826eda5b85876bcf27d0a94
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-23 12:38:59 -0800

    Add page 231 assignment.

---
+++ Pg.231Assignment/Pg.231Assignment/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg._231Assignment
+{
+    
+    
+        public class Person
+        {
+            public Person(string name) : this(name, "blue", 11)
+            {
+
+            }
+
+            public Person(string name, string favColor, int favNumber)
+            {
+                Name = name;
+                FavNumber = favNumber;
+                FavColor = favColor;
+            }
+            public string FavColor { get; set; }
+            public string Name { get; set; }
+            public int FavNumber { get; set; }
+        }
+    
+}
+    
+
---
+++ Pg.231Assignment/Pg.231Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg._231Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+    
+        
+
+                Console.WriteLine("Please enter your name");
+                string userName = Console.ReadLine();
+                var person = new Person(userName);
+                const string farewell = "have a great day";
+            Console.WriteLine(person.Name + " " + person.FavColor + " " + person.FavNumber + "\n" + farewell);
+
+                Console.ReadLine();
+            
+        }
+    }
+
+
+ 
+    
+}
