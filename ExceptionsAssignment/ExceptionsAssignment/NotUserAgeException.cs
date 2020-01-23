e07c67478eb05aa266d1ee86753bb3a6a37573ca
LewisCWarren <57116148+LewisCWarren@users.noreply.github.com>
2020-01-23 15:27:14 -0800

    Add Exceptions assignment form page 235.

---
+++ ExceptionsAssignment/ExceptionsAssignment/NotUserAgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsAssignment
+{
+    class NotUserAgeException : Exception
+    {
+        public NotUserAgeException()
+            : base() { }
+        public NotUserAgeException(string message)
+            : base() { }
+    }
+}
---
+++ ExceptionsAssignment/ExceptionsAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                Console.WriteLine("please enter your age");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                bool lieDetector = userAge > 0;
+                if (!lieDetector)
+                {
+                    throw new NotUserAgeException();
+                }
+                DateTime currentYear = new DateTime(2020);
+                DateTime userNum = new DateTime(userAge);
+                TimeSpan yearBorn = currentYear - userNum ;
+
+                
+                
+
+                Console.WriteLine(yearBorn);
+                Console.ReadLine();
+                
+            }
+            catch (NotUserAgeException)
+            {
+                Console.WriteLine("Uh oh! You've set off the BS alarm, there's no incentive to lie here...");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry, there has been an error..");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
